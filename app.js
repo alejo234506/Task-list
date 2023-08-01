0e9c19b8140cf47f495d056fd58b94c20823ba91
main <salazarquintero04@gmail.com>
2023-07-31 19:44:39 -0500

    ayudame diosito

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const input = document.getElementById('ingresar-tarea')
 const boton = document.querySelector('button')
 const listaDeTarea = document.getElementById('lista-de-tareas')
 
+
 function agregarTarea(){
     if(input.value){
         //Crear tarea
